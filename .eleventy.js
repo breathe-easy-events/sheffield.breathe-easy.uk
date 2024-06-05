4c2060d9cc14ad3a76f033b23efcd503b15b9f94
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2024-06-05 16:19:00 +0100

    process sass files

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
+const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(eleventySass);
   return {
     dir: {
       input: "src",
