df02e3d4dba02f4e6711e2a843f637e618d7407b
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2024-06-07 15:36:22 +0100

    WIP write a transform to hashfiles
    currently flaky as async functions don't run in predictable order
    and it needs the styles.css file to be there before it runs

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,34 @@
 const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
 const assetHash = require("asset-hash");
 const { Window } = require("happy-dom");
+const fs = require("fs");
 
 const dom = (content) => {
   const window = new Window();
   const document = window.document;
   document.body.innerHTML = content;
   return document;
+};
+
+const hashEl = async (el, attr, outputDir) => {
+  console.log("💜 attrubite name - " + el[attr]);
+  return await assetHash
+    // need to do some link sanatizing
+    // this fails if the plugin that processes my scss -> css doesn't beat it to this point
+    //  it's also not getting consistant hashs on the content??? which means it's not very usefull
+    .getHashedName(__dirname + outputDir + el[attr])
+    .then((name) => {
+      console.log("🧡 HASH - " + name);
+      const filePath = el[attr];
+      const link = filePath.split("/").slice(0, -1).concat(name).join("/");
+      fs.renameSync(
+        __dirname + outputDir + filePath,
+        __dirname + outputDir + link,
+      );
+      el[attr] = link;
+      return name;
+    })
+    .catch((err) => console.log(err));
 };
 
 module.exports = function (eleventyConfig) {
@@ -20,25 +42,20 @@ module.exports = function (eleventyConfig) {
       // no changes to make.
       return content;
     } else {
-      const outputDir = this.page.outputPath.split("/")[0];
-      console.log(outputDir);
-      linksToHash.forEach((el) => {
+      const outputDir = "/" + this.page.outputPath.split("/")[0];
+      for (let el of linksToHash) {
         switch (el.tagName) {
           case "LINK":
-            console.log("LINKINKINKNINKNIK");
-            console.log(el.href);
-            el.href = el.href + `?${Date.now()}`;
+            await hashEl(el, "href", outputDir);
             break;
 
           case "SCRIPT":
-            console.log("SCRIPTTTTTT");
-            console.log(el.src);
-            el.src = el.src + `?${Date.now()}`;
+            await hashEl(el, "src", outputDir);
             break;
           default:
             break;
         }
-      });
+      }
       return document.documentElement.outerHTML;
     }
   });
