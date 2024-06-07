faaf9b87117a9f20ee8a890314258eb649e69d3e
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2024-06-07 15:41:02 +0100

    quick fix for caching issues, bit of a hack

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventySass);
+  eleventyConfig.addFilter("breakCache", (value) => `${value}?${Date.now()}`);
   return {
     dir: {
       input: "src",
