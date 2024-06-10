d5d6afb24881ce179a739d894bfca510b8a0eb43
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2024-06-10 09:30:16 +0100

    WIP: hash assets
    It's working consistently the code is just an unforgivable mess

--- hash-brown.js
+++ hash-brown.js
@@ -15,19 +15,13 @@ const content = (dom) => dom.documentElement.outerHTML;
 
 const hashEl = async (el, attr, outputDir) => {
   console.log("💜 attrubite name - " + el[attr]);
+  console.log("💚 filepath - ", __dirname + "/" + outputDir + el[attr]);
   return await assetHash
     // need to do some link sanatizing
     // this fails if the plugin that processes my scss -> css doesn't beat it to this point
     //  it's also not getting consistant hashs on the content??? which means it's not very usefull
-    //
-    // A bit of digging and there's a second config phase for plugins so all the personal config stuff will run first
-    // https://www.11ty.dev/docs/transforms/#plugins
-    //
-    // So I guess the next step is to move this into a plugin and pop it at the bottom of the config
-    // I was thinking a plugin would be easier for testing but I also need to test the interaction with
-    // a project, hmmmm
 
-    .getHashedName(__dirname + outputDir + el[attr])
+    .getHashedName(__dirname + "/" + outputDir + el[attr])
     .then((name) => {
       // TODO can I check if eleventy set to --quiet ? and not log
       console.log("🧡 HASH - " + name);
@@ -40,8 +34,8 @@ const hashEl = async (el, attr, outputDir) => {
       //   __dirname + outputDir + link,
       // );
       fs.copyFileSync(
-        __dirname + outputDir + filePath,
-        __dirname + outputDir + link,
+        __dirname + "/" + outputDir + filePath,
+        __dirname + "/" + outputDir + link,
       );
       el[attr] = link;
       return name;
@@ -50,13 +44,9 @@ const hashEl = async (el, attr, outputDir) => {
 };
 
 module.exports = function (eleventyConfig, pluginOptions = {}) {
-  eleventyConfig.on(
-    "eleventy.after",
-    async ({ dir, results, runMode, outputMode }) => {
-      // Run me after the build ends
-      // console.log({ dir, results, runMode, outputMode });
+  const SELECTOR = pluginOptions.selector || "[data-asset-hash]";
 
-      /**
+  /**
 				{
 					"dir": {
 						"input": "src",
@@ -77,49 +67,64 @@ module.exports = function (eleventyConfig, pluginOptions = {}) {
 				}
 			 */
 
+  eleventyConfig.on(
+    "eleventy.after",
+    async ({ dir, results, runMode, outputMode }) => {
+      console.log("🔨BUILD DONE🔨");
+      // Because we wait till pages are built and then overwrite the templates
+      // many asset listed in the <head> and shared accross multiple pages will
+      // not need rehashing, just re-linking. So let's keep track of them here.
+      const preHashed = {};
+      // Run me after the build ends
+      // console.log({ dir, results, runMode, outputMode });
+
       // TODO
       // grab the results
-      // filter for html files
-      // filter for links to hash
-      // get all links to hash in each result obj
-      //   maybe make a cache of already processed assets so I can just update the link
-      // copy asset to file with hash in name
-      // update link in dom
-      // replace content with stringified dom
-      // overwrite html file with new content
       results
-        .filter(({ outputPath }) => outputPath.includes("html"))
-        .map((res) => ({ ...res, dom: dom(res.content) }))
-        .forEach((res) => {
+        .reduce((acc, resultObj) => {
+          if (resultObj.outputPath.includes("html")) {
+            const hdom = dom(resultObj.content);
+            const assetLinks = hdom.querySelectorAll(SELECTOR);
+            console.log({ SELECTOR });
+            console.log(assetLinks.length);
+            return assetLinks.length > 0
+              ? [...acc, { ...resultObj, dom: hdom, assetLinks }]
+              : acc;
+          } else {
+            return acc;
+          }
+        }, [])
+        .forEach(async (res) => {
+          // copy asset to file with hash in name
+          // update link in dom
+          // replace content with stringified dom
+          // overwrite html file with new content
           console.log(Object.keys(res));
+
+          const outputDir = dir.output;
+          for (let el of res.assetLinks) {
+            switch (el.tagName) {
+              case "LINK":
+                await hashEl(el, "href", outputDir);
+                break;
+
+              case "SCRIPT":
+                await hashEl(el, "src", outputDir);
+                break;
+              default:
+                break;
+            }
+          }
+          // write new content to file
+          // return document.documentElement.outerHTML;
+          fs.writeFile(res.outputPath, content(res.dom), (err) => {
+            if (err) throw err;
+            console.log("The file has been saved!");
+          });
+          console.log(
+            "🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌",
+          );
         });
     },
   );
-  console.log(pluginOptions);
-  const SELECTOR = pluginOptions.selector || "[data-asset-hash]";
-  // just going to dump this in config, consider extracting to seperate file or making a plugin if it works well
-  eleventyConfig.addTransform("asset-hash", async function (content) {
-    const document = dom(content);
-    const linksToHash = [...document.querySelectorAll(SELECTOR)];
-    if (linksToHash.length == 0) {
-      // no changes to make.
-      return content;
-    } else {
-      const outputDir = "/" + this.page.outputPath.split("/")[0];
-      for (let el of linksToHash) {
-        switch (el.tagName) {
-          case "LINK":
-            await hashEl(el, "href", outputDir);
-            break;
-
-          case "SCRIPT":
-            await hashEl(el, "src", outputDir);
-            break;
-          default:
-            break;
-        }
-      }
-      return document.documentElement.outerHTML;
-    }
-  });
 };
