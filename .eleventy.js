0c73b360065cec2f52536807cc00c8232d0699aa
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2024-06-28 14:58:33 +0100

    WIP: #9

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,11 @@
 const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
 const hashAssets = require("./src/_config/hash-assets");
+const bundleJS = require("./src/_config/bundle-javascript");
+const bundleJavascript = require("./src/_config/bundle-javascript");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventySass);
+  eleventyConfig.addPlugin(bundleJavascript, { dirname: __dirname });
   eleventyConfig.addPlugin(hashAssets, { dirname: __dirname });
   return {
     dir: {
