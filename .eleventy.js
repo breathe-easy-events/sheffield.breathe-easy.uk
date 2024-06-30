f7d88a2d6e1f9ec2092ea4cdc4b3c13e8320caa1
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2024-06-30 10:51:33 +0100

    tidy up config

--- .eleventy.js
+++ .eleventy.js
@@ -1,23 +1,20 @@
 const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
 const hashAssets = require("./src/_config/hash-assets");
 const bundleJavascript = require("./src/_config/bundle-javascript");
-const { stringIdentity } = require("./src/_config/utils");
-html = stringIdentity;
+const sectionShortcode = require("./src/_config/shortcode/section");
 
 module.exports = function (eleventyConfig) {
+  // shortcodes
+  eleventyConfig.addPlugin(sectionShortcode);
+
+  // process assets
   eleventyConfig.addPlugin(eleventySass);
   eleventyConfig.addPlugin(bundleJavascript, {
     entryPoint: "./src/js/index.ts",
     ts: true,
   });
-
-  eleventyConfig.addPairedShortcode("section", function (content) {
-    return html`<div class="section-outer">
-      <div class="section-inner">${content}</div>
-    </div>`;
-  });
-
   eleventyConfig.addPlugin(hashAssets, { dirname: __dirname });
+
   return {
     dir: {
       input: "src",
