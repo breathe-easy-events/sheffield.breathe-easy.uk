c760c3342cf0e8a818b22ee3f454750ea2609a8e
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2024-07-02 21:05:44 +0100

    remove debug statements

--- src/_config/shortcode/titleMeta.js
+++ src/_config/shortcode/titleMeta.js
@@ -1,9 +1,5 @@
 module.exports = (eleventyConfig, _) => {
   eleventyConfig.addShortcode("titleMeta", (title, page, fallback) => {
-    console.log("🔴🔴🔴🔴🔴🔴🔴🔴🔴");
-    console.log(title);
-    console.log(fallback);
-    console.log("🔴🔴🔴🔴🔴🔴🔴🔴🔴");
     if (page.url === "/") {
       return title || fallback;
     } else {
