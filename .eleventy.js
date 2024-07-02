4913eb359850343b02a10ce2df4801cbb85e845a
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2024-07-02 16:50:46 +0100

    feat: add ability to set absoluteUrl via enviroment variable

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,18 @@
 const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
 const hashAssets = require("./src/_config/hash-assets");
+const absoluteUrl = require("./src/_config/filter/absoluteUrl");
 
 module.exports = function (eleventyConfig) {
+  // templates and processing files
   eleventyConfig.addPlugin(eleventySass);
   eleventyConfig.addPlugin(hashAssets, { dirname: __dirname });
+
+  // data
+  eleventyConfig.addGlobalData("baseUrl", process.env.BASE_URL || "");
+
+  // filters
+  eleventyConfig.addPlugin(absoluteUrl);
+
   return {
     dir: {
       input: "src",
