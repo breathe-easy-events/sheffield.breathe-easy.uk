fef7fbbc595426b693d725967c538d5a5173b25f
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2024-07-04 11:58:58 +0100

    don't attempt to add baseUrl to absoluteUrls

--- src/_config/filter/absoluteUrl.js
+++ src/_config/filter/absoluteUrl.js
@@ -2,7 +2,7 @@ module.exports = (eleventyConfig, _) => {
   eleventyConfig.addFilter(
     "absoluteUrl",
     function (url, base = eleventyConfig.globalData.baseUrl) {
-      if (base) {
+      if (base && !url.startsWith("http")) {
         try {
           return new URL(url, base).href;
         } catch (err) {
